0857c0faa182b4dfd3a7822375e8127a3e693d6a
Sal <saljan93@live.com>
2020-02-24 20:00:46 +0100

    ex3

---
+++ Assignment/ocatve/ex3/costFunction.m
@@ -0,0 +1,22 @@
+function [J, grad] = costFunction(theta, X, y)
+
+%%
+m = length(y);
+J = 0;
+grad = zeros(size(theta));
+
+  
+%%
+prediction = sigmoid(X * theta);
+k = (-y .* log(prediction)) - ((1 - y) .* log(1 - prediction));
+J = sum(k) / m;
+
+
+
+%%
+error = prediction - y;
+k = X' * error;
+grad = k / m;
+
+
+endfunction
