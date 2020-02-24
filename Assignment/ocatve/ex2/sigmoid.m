427caaf47d56d077732d2fb036e25d6b88943e54
Sal <saljan93@live.com>
2020-02-24 20:00:19 +0100

    ex2

---
+++ Assignment/ocatve/ex2/sigmoid.m
@@ -0,0 +1,36 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+
+for i = 1:size(z)(1)
+    for j = 1:size(z)(2)
+      g(i, j) = 1 / (1 + exp(-z(i, j)));
+    endfor
+endfor
+
+
+% =============================================================
+
+end
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Better implementation
+%%
+%function g = sigmoid(z)
+%%SIGMOID Compute sigmoid functoon
+%%   J = SIGMOID(z) computes the sigmoid of z.
+%
+%g = 1.0 ./ (1.0 + exp(-z));
+%end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
